489f65ae528c74006b86c12504d90b5046c41512
= <rtyenriques@gmail.com>
2021-12-07 12:01:27 -0800

    commit tree 3b5b8b01ac561843961b21e1a12db9708be26640

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+ 
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newObj = {...driver};
+    newObj[key] = value;
+    return newObj
+
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+    let {[key]: omit, ...res} = driver
+    return res
+
+}
+
+const doSomething = (obj, prop) => {
+    let {[prop]: omit, ...res} = obj
+    return res
+  }
+
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+}
